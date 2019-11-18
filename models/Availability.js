99bfa2301bd259e52e161cb14383cbcf35b709b1
GregorioCunha007 <40622@alunos.isel.ipl.pt>
2019-11-18 19:56:55 +0000

    fixes in delete availabilities

--- models/Availability.js
+++ models/Availability.js
@@ -1,7 +1,6 @@
-var counter = 0;
 
 function Availability (data){
-    this._id = counter++; 
+    this._id = undefined; 
     this.weekDay = data.weekDay;
     this.startTime = data.startTime;
     this.endTime = data.endTime;
@@ -9,6 +8,11 @@ function Availability (data){
     this.equals = function(other){
         return this._id == other._id;
     }    
+
+    this.setId = function(id){
+        this._id = id;
+    }
+
     return this; 
 }
 
--- models/Professional.js
+++ models/Professional.js
@@ -3,24 +3,28 @@ const Availability = require('./Availability.js');
 function Professional(data){
     this.id = data.id;
     this.name = data.email;
-    this.availabilities = [];
+    this.availabilities = {};
+    this.availabilityCounter = 0;
 
     this.addAvailability = function(availability){
-        // Only add if it doenst exist already
-        if(!this.availabilities.find(a => a.equals(availability))) this.availabilities.push(availability);
+        //TODO: Don't add availabilities with the same options
+        this.availabilities[this.availabilityCounter] = availability;
+        availability.setId(this.availabilityCounter);
+        ++this.availabilityCounter;
         return this;
     }
     
     this.deleteAvailability = function(_id){
-        // The _id of Availability also is the index position in the array
-        this.availabilities.splice(_id, 1);
+        // Expensive operation, only doing it because don't want to return undefined positions
+        delete this.availabilities[_id];
         return this;
     }
 
     this.updateAvailability = function(_id, data){
-        this.availabilities[_id].weekDay = data.weekDay;
-        this.availabilities[_id].startTime = data.startTime;
-        this.availabilities[_id].endTime = data.endTime;
+        let availability = this.availabilities[_id];
+        availability.weekDay = data.weekDay;
+        availability.startTime = data.startTime;
+        availability.endTime = data.endTime;
         return this;
     }
 
